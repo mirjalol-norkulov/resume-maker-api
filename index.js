43e57751af6f1d6e491adf0bd0d41418a0c844d9
Mirjalol Norqulov <mirjalol2401@gmail.com>
2021-06-23 21:52:48 +0500

    get port number from env variable

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const puppeteer = require('puppeteer');
 
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json({extended: true, limit: '50mb'}))
