40957232bcaacd7a3aa9608a2edf96883ce4de07
Mirjalol Norqulov <mirjalol2401@gmail.com>
2021-06-23 22:17:20 +0500

    update cors origins

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ app.use(bodyParser.json({extended: true, limit: '50mb'}))
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.setHeader(
+        'Access-Control-Allow-Origin',
+        ['http://localhost:8080', 'https://resume-maker-uz.netlify.app', 'https://resume.mirjalolnorkulov.uz']
+    );
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
